428ca5e6f7a69590e3cafaa555e07ecd918825c0
Simon Gowing <simongowing1@gmail.com>
2021-02-05 10:39:56 +0100

    second commit

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/*
+let today = new Date();
+let dd = String(today. getDate()). padStart(2, '0');
+let mm = String(today. getMonth() + 1). padStart(2, '0');
+let yyyy = today. getFullYear();​
+
+today = mm + '/' + dd + '/' + yyyy;
+*/
+
 const recipeSchema = new Schema({
   title: String,
   level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
@@ -10,7 +19,7 @@ const recipeSchema = new Schema({
   image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
   duration: { type: Number, min: 0 },
   creator: String,
-  created: {Date, default: }
+  created: Date
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
